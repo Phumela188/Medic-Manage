586355843d8d2fe70dc4d118bc172ae3df7d87c5
zachaareee <zacharyjaptha13@gmail.com>
2025-07-29 12:12:31 +0200

    Finished the daos

--- app/src/main/java/com/example/myapplication/AppointmentDAO.java
+++ app/src/main/java/com/example/myapplication/AppointmentDAO.java
@@ -1,6 +1,25 @@
 package com.example.myapplication;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface AppointmentDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertAppointment(Appointment appointment);
+
+    @Query("select * from appointment")
+    public List<Appointment> getAllAppointments();
+
+    @Query("select * from appointment where `Appointment Num`= `Appointment Num`")
+    public Appointment getAppointmentName(int appointmentNum);
+
+
+
 
 
 }
--- app/src/main/java/com/example/myapplication/Appointment_MedicationDAO.java
+++ app/src/main/java/com/example/myapplication/Appointment_MedicationDAO.java
@@ -1,4 +1,19 @@
 package com.example.myapplication;
 
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
 public interface Appointment_MedicationDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insert(Appointment_Medication appointmentMedication);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertAll(List<Appointment_Medication> entries);
+
+    @Query("select * from `appointment medication` where appointmentNum = appointmentNum")
+    public List<Appointment_Medication> getMedicationForAppointment(int appointmentNum);
 }
